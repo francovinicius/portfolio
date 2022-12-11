1e480b8843349087ea13b18cbf8b7219ccca3eb6
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2022-12-11 17:52:44 -0300

    adicionado a funcionalidade no botao do menu

---
+++ assets/js/esconderMenu.js
@@ -0,0 +1,6 @@
+const menuNav = document.querySelector('.menu-navbar');
+const menuLateral = document.querySelector('.menu-lateral');
+
+menuNav.addEventListener('click', () => {
+    menuLateral.classList.toggle('menu-lateral-ativo');
+})

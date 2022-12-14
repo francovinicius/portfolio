5271c9b7738ba4d055073b354dd255a2571dae38
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2022-12-14 16:56:40 -0300

    Criação do botão ler mais que esconde o sobre mim completo deixando somente a versao resumida

---
+++ assets/js/lerMais.js
@@ -0,0 +1,8 @@
+const aparecendoSobre = document.querySelector ('.ler-mais');
+const lerMaisBtn = document.querySelector('.btn-ler-mais');
+
+
+lerMaisBtn.addEventListener('click', () => {
+    aparecendoSobre.classList.toggle('.ler-mais-ativo');
+    console.log('funcionou');
+})

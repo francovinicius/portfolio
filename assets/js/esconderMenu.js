c3d756156d45793ab41045a7d1d62641f11f0a36
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2022-12-14 10:45:51 -0300

    recomeçando utilizando o bootstrap

--- assets/js/esconderMenu.js
+++
@@ -1,6 +0,0 @@
-const menuNav = document.querySelector('.menu-navbar');
-const menuLateral = document.querySelector('.menu-lateral');
-
-menuNav.addEventListener('click', () => {
-    menuLateral.classList.toggle('menu-lateral-ativo');
-})
